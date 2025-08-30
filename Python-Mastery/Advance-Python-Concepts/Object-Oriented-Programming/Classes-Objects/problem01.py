be16eff286a545d2adc4fbdb780ef9e08c13645b
Gopal Mahato <gopalmahatomain2026@gmail.com>
2025-08-30 08:23:30 +0530

    Adding two problems

--- Python-Mastery/Advance-Python-Concepts/Object-Oriented-Programming/Classes-Objects/problem01.py
+++ Python-Mastery/Advance-Python-Concepts/Object-Oriented-Programming/Classes-Objects/problem01.py
@@ -2,7 +2,22 @@
 ## Practice Problems
 - 1. Create a class Person with attributes name and age . Initialize them in the constructor and
 add a method greet that prints a greeting message including the name and age.
-
 """
-print("My name is Gopal Mahato!")
-print("I am using sublime as an text editor!")
+
+class Person:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age 
+
+	def greet(self):
+		return f"Hello {self.name} You are {self.age} years old!"
+
+
+p1 = Person("Gopal Mahato", 19)
+print(p1.greet())
+
+p2 = Person("Ujjwal Das", 19)
+print(p2.greet())
+
+p3 = Person("Yubraj Sing Sardar", 21)
+print(p3.greet())
